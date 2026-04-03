96e93724f0a8716fa7c9a255209eb9129b396596
kramedavid@gmail.com <kramedavid@gmail.com>
2026-04-03 19:24:10 +0200

    Progress bar and News page

---
+++ assets/js/progress.js
@@ -0,0 +1,12 @@
+(function () {
+  var bar = document.getElementById('progress-bar');
+  if (!bar) return;
+  function update() {
+    var scrollTop = window.scrollY || document.documentElement.scrollTop;
+    var docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    var pct = docHeight > 0 ? (scrollTop / docHeight) * 100 : 0;
+    bar.style.width = pct + '%';
+  }
+  window.addEventListener('scroll', update, { passive: true });
+  update();
+})();
